a01b3b162e2bfed691c052c27f4f9e67686de61b
[AloneAsif] <[asifleon3@gmail.com]>
2024-03-06 19:54:09 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+console.log('Ali Ibn Abi Talib, "The disease of the heart is worse than the disease of the body."');
+// Question no # 5          Famous Quote 2:
+var famous_person = 'Ali Ibn Abi Talib';
+var message = '"The disease of the heart is worse than the disease of the body."';
+console.log("".concat(famous_person, ", ").concat(message));
---
+++ main.ts
@@ -0,0 +1,7 @@
+console.log('Ali Ibn Abi Talib, "The disease of the heart is worse than the disease of the body."');
+
+// Question no # 5          Famous Quote 2:
+
+let famous_person: string = 'Ali Ibn Abi Talib';
+let message: string = '"The disease of the heart is worse than the disease of the body."';
+console.log(`${famous_person}, ${message}`);
